033257f194adfa52ff31b9894ec9cac0c44484d4
Alex Ibrado <alex@ibrado.org>
2018-01-18 07:09:00 +0800

    Improvement lexer for TT

--- lib/jekyll/viewsource/lexer.rb
+++ lib/jekyll/viewsource/lexer.rb
@@ -21,10 +21,40 @@ module Rouge
         rule(%r[\s*(?<!:)//.*?$]) { delegate javascript }
         rule(%r(\s*/[*].*?[*]/\s*)m) { delegate javascript }
 
-        rule(%r(.*?%}.?)) { delegate liquid }
-        rule(%r(.*{{.*?}}.?)) { delegate liquid }
-
         rule(%r(\s*<!--.*?-->\s*)m) { delegate html }
+
+        rule %r(({%)(\s*)(macro)(\s+)(/.*?/\S*)(\s*)({)) do
+          groups Punctuation, Text::Whitespace, Name::Tag, Text::Whitespace, Str::Double, Text::Whitespace, Punctuation
+        end
+
+        rule %r((})(\s*)(%})) do
+          groups Punctuation, Text::Whitespace, Punctuation
+        end
+
+        rule %r(({%)(\s*)(constant)(\s+)(\S+)(\s*)(=?)(\s*)(['"]?)(.*?)(['"]?)(\s*)(%})) do
+          groups Punctuation, Text::Whitespace, Name::Tag, Text::Whitespace, 
+            Name::Attribute, Text::Whitespace, Punctuation, Text::Whitespace,
+            Punctuation, Str::Double, Punctuation, Text::Whitespace, Punctuation
+        end
+
+        rule %r((\s*)({%)(\s*)(hashkeys)(\s+)(\S+)(\s*)(%})) do
+          groups Text::Whitespace, Punctuation, Text::Whitespace, Name::Tag,
+          Text::Whitespace, Name::Attribute, Text::Whitespace, Punctuation
+        end
+
+        rule %r((\s*)({%)(\s*)(hashkeys)(\s+)(\S+)(\s*)({)(.*?)(})(\s*)(%})) do
+          groups Text::Whitespace, Punctuation, Text::Whitespace,
+          Name::Tag, Text::Whitespace, Name::Attribute, Text::Whitespace,
+          Punctuation, Text, Punctuation, Text::Whitespace, Punctuation
+        end
+
+        rule %r(({%)(\s*)(project)(\s*)(.*?)(\s*)(%})) do
+          groups Punctuation, Text::Whitespace, Name::Tag, Text::Whitespace, Text,
+            Text::Whitespace, Punctuation
+        end
+
+        rule(%r(.*?{%.*?%}.?)) { delegate liquid }
+        rule(%r(.*{{.*?}}.?)) { delegate liquid }
       end
 
     end
